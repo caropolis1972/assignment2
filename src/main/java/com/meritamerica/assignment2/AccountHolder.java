090b4f6f039e3c1744de07b252c3c3ff9d9dc9e2
Matt Chvatal <mdchvatal@gmail.com>
2021-03-04 13:24:29 -0500

    beginning code structure

--- src/main/java/com/meritamerica/assignment2/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment2/AccountHolder.java
@@ -3,4 +3,41 @@ package com.meritamerica.assignment2;
 import java.util.Arrays;
 
 public class AccountHolder {
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private int numberOfCheckingAccounts;
+	
+	public AccountHolder(String firstName, String middleName, String lastName, String ssn) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.ssn = ssn;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
 }
